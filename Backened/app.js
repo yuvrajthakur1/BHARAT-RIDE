08931aa85d256cf51c3f93f4b08cc2864ba72ec3
yuvrajthakur1 <yuvrajthakurthakur65@gmail.com>
2025-01-26 09:42:03 +0530

    first commit

---
+++ Backened/app.js
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+
+app.get('/',(req,res)=>{
+  res.send("Hello World");
+})
+
+
+module.exports = app;
